e70986c512b466e2a46228fbf7427db88729ee89
jaychang99 <jaychang99@gmail.com>
2023-01-28 13:20:21 +0900

    feat: Dropdown 컴포넌트 생성

---
+++ src/components/common/Dropdown.tsx
@@ -0,0 +1,47 @@
+import { createStyles, Select, SelectProps } from '@mantine/core';
+import ArrowDownIcon from 'components/common/icons/ArrowDownIcon';
+import React from 'react';
+
+interface Props extends SelectProps {
+  kind?: 'default' | 'big';
+}
+
+// usage
+{
+  /* <Dropdown
+label="Your favorite framework/library"
+placeholder="Pick one"
+data={[
+  { value: 'react', label: 'React' },
+  { value: 'ng', label: 'Angular' },
+  { value: 'svelte', label: 'Svelte' },
+  { value: 'vue', label: 'Vue' },
+]}
+/> */
+}
+
+function Dropdown({ kind = 'default', ...props }: Props) {
+  return (
+    <Select
+      rightSection={<ArrowDownIcon />}
+      rightSectionWidth={50}
+      styles={(theme) => ({
+        input: {
+          padding:
+            kind === 'default' ? '10px 18px 11px 20px' : '14px 24px 15px 20px',
+          height: 'auto',
+          lineHeight: '100%',
+          fontSize: '15px',
+        },
+        rightSection: { pointerEvents: 'none' },
+        dropdown: {
+          // dropdown 아이템들을 포함하는 container
+          // TODO: 이거 간격 7px 로 조정하기
+        },
+      })}
+      {...props}
+    />
+  );
+}
+
+export default Dropdown;
