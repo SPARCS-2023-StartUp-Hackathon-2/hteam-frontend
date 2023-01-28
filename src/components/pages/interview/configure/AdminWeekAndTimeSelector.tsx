e853a443ada1070bb4073f484bc745e55427a220
jaychang99 <jaychang99@gmail.com>
2023-01-28 17:19:01 +0900

    feat: 일단 일정 관련 쪽 완료

--- src/components/pages/interview/configure/AdminWeekAndTimeSelector.tsx
+++ src/components/pages/interview/configure/AdminWeekAndTimeSelector.tsx
@@ -1,4 +1,4 @@
-import { Box, Checkbox, Flex } from '@mantine/core';
+import { Box, Checkbox, Flex, NumberInput } from '@mantine/core';
 import { TimeInput } from '@mantine/dates';
 import React from 'react';
 
@@ -14,15 +14,25 @@ function WeekdayAndTimeRow({ dayOfTheWeek }: { dayOfTheWeek: string }) {
 
 function AdminWeekAndTimeSelector() {
   return (
-    <Box
+    <Flex
+      direction="row"
+      align="center"
       sx={(theme) => ({
         backgroundColor: theme.white,
-        padding: '32px',
+
         border: `1px solid ${theme.colors.gray[1]}`,
         borderRadius: theme.radius.sm,
       })}
     >
-      <Flex gap="20px" direction="column" align="center">
+      <Flex
+        gap="20px"
+        direction="column"
+        align="center"
+        sx={(theme) => ({
+          borderRight: `1px solid ${theme.colors.gray[1]}`,
+          padding: '32px',
+        })}
+      >
         <WeekdayAndTimeRow dayOfTheWeek="일" />
         <WeekdayAndTimeRow dayOfTheWeek="월" />
         <WeekdayAndTimeRow dayOfTheWeek="화" />
@@ -31,7 +41,53 @@ function AdminWeekAndTimeSelector() {
         <WeekdayAndTimeRow dayOfTheWeek="금" />
         <WeekdayAndTimeRow dayOfTheWeek="토" />
       </Flex>
-    </Box>
+      <Box>
+        <Box>
+          <Flex
+            gap="20px"
+            direction="row"
+            align="center"
+            sx={(theme) => ({
+              borderBottom: `1px solid ${theme.colors.gray[1]}`,
+              padding: '32px',
+            })}
+          >
+            인터뷰 진행 시간
+            <NumberInput
+              defaultValue={30}
+              placeholder="Your age"
+              hideControls
+            />
+          </Flex>
+        </Box>
+        <Box>
+          <Flex
+            gap="20px"
+            direction="row"
+            align="center"
+            sx={(theme) => ({
+              padding: '32px',
+            })}
+          >
+            인터뷰 준비 시간
+            <Flex gap="20px" direction="column" align="center">
+              <NumberInput
+                defaultValue={30}
+                label="인터뷰 전"
+                placeholder="인터뷰 전"
+                hideControls
+              />
+              <NumberInput
+                defaultValue={30}
+                label="인터뷰 후"
+                placeholder="인터뷰 후"
+                hideControls
+              />
+            </Flex>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
 
